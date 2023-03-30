80829f7d1f2e93a02dbd45892805a33f48978a37
SetKaung <zetaprotocol8@gmail.com>
2023-03-30 15:14:58 +0630

    working, for now

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
-const choiceMp = new Map()
-choiceMp.set(0, 'Rock')
-choiceMp.set(1,'Paper')
-choiceMp.set(2,'Scissor')
+const choiceMp = new Map();
+choiceMp.set(0, 'Rock');
+choiceMp.set(1, 'Paper');
+choiceMp.set(2, 'Scissor');
 function getComputerChoice() {
-    return Math.floor(Math.random()*1 * Math.random()*3) 
+    return Math.floor(Math.random() * 1 * Math.random() * 3)
+};
 
+let playerScore = 0;
+let computerScore = 0;
+
+
+let winnerDecl = document.querySelector(".winner-declaration");
+let playerChoice = document.querySelector(".player-choice");
+let computerChoice = document.querySelector(".computer-choice");
+let playerScoreText = document.querySelector(".player-score");
+let computerScoreText = document.querySelector(".computer-score");
+
+
+
+function winning(winner){
+    if(winner === "Player"){
+        playerScore++;
+        playerScoreText.textContent = "Player: " + playerScore;
+
+    }else{
+        computerScore++;
+        computerScoreText.textContent = "Computer:" + computerScore;
+    }
+
+    if(playerScore === 5){
+        winnerDecl.textContent = "You win! :)";
+    }
+    else if(computerScore === 5){
+        winnerDecl.textContent = "The Computer wins! :((";
+    }
 }
 
-//this funciton mapped 0 to rock, 1 to paper and 2 to scissor
-function getInput(){
-    let input = prompt("Enter Rock,Paper or Scissor")
-    while(input===""){
-        input = prompt("Please enter a choice!")
-    }
-    input = input.toLowerCase()
-    if(input === "rock"){
-        return 0
-    }
-    else if(input === "paper"){
-        return 1
-    }
-    else{
-        return 2
-    }
-}
 
-/*
-the mapping to numbers solve the issues of writing too many conditional statements.
-but since the graph of win conditions is circular, it is required to adjust two of the values.
-the %2!=0 check whether it is a scissor and rock condition in this case.
-*/
 
-function game(){
+// /*
+// the mapping to numbers solve the issues of writing too many conditional statements.
+// but since the graph of win conditions is circular, it is required to adjust two of the values.
+// the %2!=0 check whether it is a scissor and rock condition in this case.
+// */
+
+function game(input) {
     const mp = new Map()
-    mp.set('player',getInput())
-    mp.set('computer',getComputerChoice())
+    mp.set('player', input)
+    mp.set('computer', getComputerChoice())
 
-    console.log("The computer choosed: "+ choiceMp.get(mp.get('computer')))
-    console.log("You choosed: " + choiceMp.get(mp.get('player')))
+    playerChoice.textContent = "You choosed " + choiceMp.get(mp.get('player'));
+    computerChoice.textContent = "The computer choosed " + choiceMp.get(mp.get('computer'));
     
-    if(mp.get('player') === mp.get('computer')){
-        console.log("It's a tie!")
+
+    if (mp.get('player') === mp.get('computer')) {
+        winnerDecl.textContent = "It's a tie!";
     }
-    else if((mp.get('player')+mp.get('computer'))%2 != 0){
+
+    else if ((mp.get('player') + mp.get('computer')) % 2 != 0) {
         winner = mp.get('player') > mp.get('computer') ? "Player" : "Computer"
-        console.log("The winner is " + winner)
+        winning(winner);
     }
-    else{
+    else {
         winner = mp.get('player') < mp.get('computer') ? "Player" : "Computer"
-        console.log("The winner is " + winner)
+        winning(winner);
     }
-    
+
 }
 
-game()
+
+let rockBtn = document.querySelector(".rock");
+rockBtn.addEventListener("click", () => {game(0)});
+
+let paperBtn = document.querySelector(".paper");
+paperBtn.addEventListener("click", () => {game(1)});
+
+let scissorBtn = document.querySelector(".scissor");
+scissorBtn.addEventListener("click", () => {game(2)})
