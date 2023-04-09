21433591523a3ff0d62c71d8a58d4b6a5e564818
SetKaung <zetaprotocol8@gmail.com>
2023-04-09 15:21:59 +0630

    final version

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function getComputerChoice() {
 
 let playerScore = 0;
 let computerScore = 0;
+let roundCounter = 0;
+
 
 
 let winnerDecl = document.querySelector(".winner-declaration");
@@ -15,28 +17,56 @@ let playerChoice = document.querySelector(".player-choice");
 let computerChoice = document.querySelector(".computer-choice");
 let playerScoreText = document.querySelector(".player-score");
 let computerScoreText = document.querySelector(".computer-score");
+let buttonContainer = document.querySelector(".button-container");
+let playBtn = document.querySelector(".play");
+let record = document.querySelector(".record");
+
+buttonContainer.style.display = "none";
+record.style.display = "none";
 
 
 
-function winning(winner){
-    if(winner === "Player"){
-        playerScore++;
-        playerScoreText.textContent = "Player: " + playerScore;
 
-    }else{
-        computerScore++;
-        computerScoreText.textContent = "Computer:" + computerScore;
-    }
 
-    if(playerScore === 5){
-        winnerDecl.textContent = "You win! :)";
-    }
-    else if(computerScore === 5){
-        winnerDecl.textContent = "The Computer wins! :((";
-    }
+function playing(){
+    buttonContainer.style.display ="block";
+    playBtn.style.display = "none";
+    record.style.display = "block";
 }
 
+function ending(winner){
+    buttonContainer.style.display = "none";
+    playBtn.style.display = "block";
+    winnerDecl.textContent = "Winner is:" + winner;
+    playerScore = 0;
+    computerScore = 0;
+    playerChoice.textContent = "";
+    computerChoice.textContent = "";
+    record.style.display = "none";
+}
 
+function updateDisplay(){
+    playerScoreText.textContent = "Player Score:" + playerScore;
+    computerScoreText.textContent = "Computer Score:" + computerScore;
+
+}
+
+function winning(winner){
+        if(winner === "Player"){
+            playerScore++;
+            updateDisplay();
+            if(playerScore == 5){
+                ending("Player");
+            }
+        }
+        else if(winner == "Computer"){
+            computerScore++;
+            updateDisplay();
+            if(computerScore == 5){
+                ending("Computer");
+            }
+        }
+}
 
 // /*
 // the mapping to numbers solve the issues of writing too many conditional statements.
@@ -44,17 +74,18 @@ function winning(winner){
 // the %2!=0 check whether it is a scissor and rock condition in this case.
 // */
 
-function game(input) {
+function round(input) {
     const mp = new Map()
     mp.set('player', input)
     mp.set('computer', getComputerChoice())
 
-    playerChoice.textContent = "You choosed " + choiceMp.get(mp.get('player'));
-    computerChoice.textContent = "The computer choosed " + choiceMp.get(mp.get('computer'));
+
+    playerChoice.textContent = "Player choosed: " + choiceMp.get(mp.get('player'));
+    computerChoice.textContent = "Computr choosed: " + choiceMp.get(mp.get('computer'));
     
 
-    if (mp.get('player') === mp.get('computer')) {
-        winnerDecl.textContent = "It's a tie!";
+    if(mp.get('player') === mp.get('computer')) {
+
     }
 
     else if ((mp.get('player') + mp.get('computer')) % 2 != 0) {
@@ -69,11 +100,14 @@ function game(input) {
 }
 
 
+
 let rockBtn = document.querySelector(".rock");
-rockBtn.addEventListener("click", () => {game(0)});
+rockBtn.addEventListener("click", () => {round(0)});
 
 let paperBtn = document.querySelector(".paper");
-paperBtn.addEventListener("click", () => {game(1)});
+paperBtn.addEventListener("click", () => {round(1)});
 
 let scissorBtn = document.querySelector(".scissor");
-scissorBtn.addEventListener("click", () => {game(2)})
+scissorBtn.addEventListener("click", () => {round(2)});
+
+playBtn.addEventListener("click", playing);
